1e14d702ea13bd1d3062a267b1e95c472b44e876
Ales Zagar <ales.zagar@hotmail.com>
2022-09-13 11:04:59 +0200

    updated ports in README.md and commands.sh

--- main.sh
+++ main.sh
@@ -16,39 +16,39 @@ cd "$ROOT"/src/metamodel
 conda create -n metamodel-local python=3.8
 conda activate metamodel-local
 pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8000 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5003 &
 
 # graph-based
 cd "$ROOT"/src/graph-based
 conda create -n graph-based-local python=3.8
 conda activate graph-based-local
 pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8001 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5004 &
 
 # t5-headline
 cd "$ROOT"/src/t5-headline
 conda create -n t5-headline-local python=3.8
 conda activate t5-headline-local
 pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8002 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5005 &
 
 # t5-article
 cd "$ROOT"/src/t5-article
 conda create -n t5-article-local python=3.8
 conda activate t5-article-local
 pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8003 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5006 &
 
 # sumbasic
 cd "$ROOT"/src/sumbasic
 conda create -n sumbasic-local python=3.8
 conda activate sumbasic-local
 pip3 install -r requirements.txt
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8004 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5007 &
 
 # hybrid long
 cd "$ROOT"/src/hybrid-long
 conda create -n hybrid-long-local python=3.8
 conda activate hybrid-long-local
 pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-uvicorn main-fastapi:app --host 0.0.0.0 --port 8005 &
+uvicorn main-fastapi:app --host 0.0.0.0 --port 5008 &
--- src/metamodel/README.md
+++ src/metamodel/README.md
@@ -3,11 +3,11 @@ To run this project, install python 3.8 and dependencies:
 `pip3 install -r requirements.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html`
 
 ### Run server
- `uvicorn main-fastapi:app --host 0.0.0.0 --port 8000`
+ `uvicorn main-fastapi:app --host 0.0.0.0 --port 5003`
 To test the service, try sending a request with the curl command provided in the file `commands.sh`
 
 # Docker
 `docker buildx build --platform linux/amd64 . -t metamodel -f Dockerfile`
 
-`docker run --rm --platform linux/amd64 -it --name metamodel -p:8000:8000 metamodel`
+`docker run --rm --platform linux/amd64 -it --name metamodel -p:5003:5003 metamodel`
 
